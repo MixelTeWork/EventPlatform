74e01036dc1571ed6042e770eec19a67c76dfaeb
Mike <63655075+MixelTe@users.noreply.github.com>
2024-01-14 15:12:17 +0300

    init backend

---
+++ backend/data/operation.py
@@ -0,0 +1,36 @@
+from sqlalchemy import Column, String
+from sqlalchemy_serializer import SerializerMixin
+from .db_session import SqlAlchemyBase
+
+
+class Operation(SqlAlchemyBase, SerializerMixin):
+    __tablename__ = "Operation"
+
+    id   = Column(String(32), primary_key=True, unique=True)
+    name = Column(String(32), nullable=False)
+
+    def __repr__(self):
+        return f"<Operation> {self.id} {self.name}"
+
+    # def get_dict(self):
+    #     return self.to_dict(only=("id", "name"))
+
+
+class Operations:
+    # pages
+    page_users = ("page_users", "Страница пользователей")
+    page_debug = ("page_debug", "Страница отладки")
+
+    # get
+
+    # add
+
+    # change
+
+    # delete
+
+    @staticmethod
+    def get_all():
+        obj = Operations()
+        members = [attr for attr in dir(obj) if not callable(getattr(obj, attr)) and not attr.startswith("__")]
+        return map(lambda x: getattr(obj, x), members)
---
+++ backend/scripts/init_values.py
@@ -0,0 +1,91 @@
+import sys
+import os
+
+
+def init_values(dev=False, cmd=False):
+    if dev:
+        if not os.path.exists("db"):
+            os.makedirs("db")
+        if cmd:
+            add_parent_to_path()
+    else:
+        add_parent_to_path()
+
+    from datetime import timedelta
+    from random import randint, choice
+    from data import db_session
+    from data.log import Actions, Log, Tables
+    from data.operation import Operation, Operations
+    from data.permission import Permission
+    from utils.randstr import randstr
+    from data.role import Role, Roles, ROLES
+    from data.user import User
+    from utils import get_datetime_now
+
+    def init():
+        db_session.global_init("dev" in sys.argv)
+        db_sess = db_session.create_session()
+
+        for operation in Operations.get_all():
+            db_sess.add(Operation(id=operation[0], name=operation[1]))
+
+        roles = []
+        for role_id, role_data in ROLES.items():
+            role_name = role_data["name"]
+            role = Role(name=role_name, id=role_id)
+            roles.append(role)
+            db_sess.add(role)
+
+            for operation in role_data["operations"]:
+                db_sess.add(Permission(roleId=role_id, operationId=operation[0]))
+
+        role_admin = Role(name="Админ", id=Roles.admin)
+        roles.append(role_admin)
+        db_sess.add(role_admin)
+
+        for operation in Operations.get_all():
+            db_sess.add(Permission(roleId=Roles.admin, operationId=operation[0]))
+
+        user_admin = User.new(db_sess, User(id=1, name="Админ"), "admin", "admin", "Админ", [Roles.admin])
+        user_admin.set_password("admin")
+        db_sess.add(user_admin)
+        db_sess.commit()
+
+        log_changes(db_sess, user_admin, roles)
+
+        if dev:
+            init_values_dev(db_sess, user_admin)
+
+    def log_changes(db_sess, user_admin, roles):
+        now = get_datetime_now()
+
+        def log(tableName, recordId, changes):
+            db_sess.add(Log(
+                date=now,
+                actionCode=Actions.added,
+                userId=user_admin.id,
+                userName=user_admin.name,
+                tableName=tableName,
+                recordId=recordId,
+                changes=changes
+            ))
+
+        for role in roles:
+            log(Tables.Role, role.id, role.get_creation_changes())
+
+        db_sess.commit()
+
+    def init_values_dev(db_sess, user_admin):
+        pass
+
+    init()
+
+
+def add_parent_to_path():
+    current = os.path.dirname(os.path.realpath(__file__))
+    parent = os.path.dirname(current)
+    sys.path.append(parent)
+
+
+if __name__ == "__main__":
+    init_values("dev" in sys.argv, True)
