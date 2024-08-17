46544cdf5bc97de7615461daa44b7c0e9221db80
Mike <63655075+MixelTe@users.noreply.github.com>
2024-08-17 13:06:01 +0300

    change title of auth page

--- frontend/src/pages/AuthPage/index.tsx
+++ frontend/src/pages/AuthPage/index.tsx
@@ -25,7 +25,8 @@ export default function AuthPage()
 	return (
 		<Layout header={null} centered centeredPage gap="2em">
 			<Link to="/" className={styles.back}></Link>
-			<h1>Мероприятие</h1>
+			<h3>Получено достижение!</h3>
+			<h1>Вы не должны были это увидеть</h1>
 			{error && <h3>{error}</h3>}
 			{mutation.isLoading && <Spinner />}
 			<Form className={styles.form} onSubmit={onSubmit}>
