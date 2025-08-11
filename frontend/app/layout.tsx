9c99850283085843f7ac14f21f6f40dceaeff0c8
Mike <63655075+MixelTe@users.noreply.github.com>
2025-08-11 23:22:17 +0300

    fix frontend

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -8,16 +8,16 @@ import MessageFromBackend from "@/components/MessageFromBackend";
 
 const font_PTSans = localFont({
 	src: [
-		{ path: "../public/fonts/PT_Sans/PTSans-Regular.ttf", weight: "400", style: "normal", },
-		{ path: "../public/fonts/PT_Sans/PTSans-Italic.ttf", weight: "400", style: "italic", },
-		{ path: "../public/fonts/PT_Sans/PTSans-Bold.ttf", weight: "700", style: "normal", },
-		{ path: "../public/fonts/PT_Sans/PTSans-BoldItalic.ttf", weight: "700", style: "italic", },
+		{ path: "../fonts/PT_Sans/PTSans-Regular.ttf", weight: "400", style: "normal", },
+		{ path: "../fonts/PT_Sans/PTSans-Italic.ttf", weight: "400", style: "italic", },
+		{ path: "../fonts/PT_Sans/PTSans-Bold.ttf", weight: "700", style: "normal", },
+		{ path: "../fonts/PT_Sans/PTSans-BoldItalic.ttf", weight: "700", style: "italic", },
 	],
 	variable: "--font-PTSans",
 });
 
-const font_ZeroCool = localFont({ src: "../public/fonts/ZeroCool.ttf", variable: "--font-ZeroCool", fallback: ["var(--font-PTSans)"], adjustFontFallback: false });
-const font_MFFRBL = localFont({ src: "../public/fonts/monsterfriendforerusbylya.otf", variable: "--font-MFFRBL", fallback: ["var(--font-PTSans)"], adjustFontFallback: false });
+const font_ZeroCool = localFont({ src: "../fonts/ZeroCool.ttf", variable: "--font-ZeroCool", fallback: ["var(--font-PTSans)"], adjustFontFallback: false });
+const font_MFFRBL = localFont({ src: "../fonts/monsterfriendforerusbylya.otf", variable: "--font-MFFRBL", fallback: ["var(--font-PTSans)"], adjustFontFallback: false });
 
 const url = new URL(process.env.PUBLIC_URL || "");
 export const metadata: Metadata = {
